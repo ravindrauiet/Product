d89049f274dff935ba931d5e12a6a45285b2e999
Jay Jha <16550695+jayanand01@users.noreply.github.com>
2024-05-26 21:42:59 +1000

    filereader

--- src/pages/ProductsForm.jsx
+++ src/pages/ProductsForm.jsx
@@ -1,43 +1,53 @@
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-// import { open, close } from 'node:fs';
+import Form from 'react-bootstrap/Form'
 
-const fs = require('fs');
-
+const saveFile = async (blob) => {
+    const a = document.createElement('a');
+    a.download = 'my-file.txt';
+    a.href = URL.createObjectURL(blob);
+    a.addEventListener('click', (e) => {
+      setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+    });
+    a.click();
+  };
+  const obj = {hello: 'world'};
+  const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
+  
+  saveFile(blob);
+  
+const showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      console.log(text)
+      alert(text)
+    };
+    reader.readAsText(e.target.files[0])
+  }
 
 export function ProductsForm(props) {
     const [errorMessage, setErrorMessage] = useState("");
-    const [validated, setValidated] = useState(false);
 
     function handleSubmit(event) {
         event.preventDefault();
-        event.stopPropagation();
 
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            setValidated(true);
+        //read form data
+        const formData = new FormData(event.target);
+        console.log(formData)
+
+        //convert form Data to object
+        const product = Object.fromEntries(formData.entries());
+
+        //Form validation
+        if (!product.party_name) {
+            setErrorMessage(
+                <div className="alert alert-warning" role="alert">
+                    please provide all required fields!
+                </div>
+            )
             return;
         }
-        // console.log(form);
-        
-        // Read form data
-        const formData = new FormData(form);
-        // console.log(formData);
-        
-        // Convert formData to object
-        const product = Object.fromEntries(formData.entries());
-        console.log(product);
-        // Add createdAt field for new products
-        if (!props.product.id) {
-            product.createdAt = new Date().toISOString().slice(0, 10);
-        }
-
-
-        // Set validation state
-        setValidated(true);
 
         if (props.product.id) {
             //call update method from props
@@ -46,7 +56,7 @@ export function ProductsForm(props) {
                 headers: {
                     "content-type": "application/json",
                 },
-                body: JSON.stringify(product),
+                body: JSON.stringify(product)
             })
                 .then((response) => {
                     if (!response.ok) {
@@ -69,8 +79,7 @@ export function ProductsForm(props) {
                 headers: {
                     "content-type": "application/json",
                 },
-                // body: JSON.stringify(product)
-                fs.writeFileSync('data.json', JSON.stringify(product));
+                body: JSON.stringify(product)
             })
                 .then((response) => {
                     if (!response.ok) {
@@ -84,101 +93,94 @@ export function ProductsForm(props) {
                 })
                 .catch((error) => console.error('Error:', error));
         }
-     }
-
+    }
     return (
         <>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <Row className="mb-1">
-                    <Form.Group as={Col} sm="1" controlId="validationCustom01">
-                        <Form.Label>Sl_No</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            name="sl_no"
-                            placeholder="Sl_No"
-                            defaultValue=""
-                        />
-                    </Form.Group>
-                    <Form.Group as={Col} sm="1" controlId="validationCustom02">
-                        <Form.Label>Sales_Purchase</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            name="sales_purchase"
-                            placeholder="Sales"
-                            defaultValue=""
-                        />
-                    </Form.Group>
-                
-                    <Form.Group as={Col} sm="1" controlId="validationCustom03">
-                        <Form.Label>Date</Form.Label>
-                        <Form.Control type="text" name="date" placeholder="Date" defaultValue="" />
-                    </Form.Group>
+            {/* <h2 className="text-center mb-3">{props.product.id ? "Edit Product" : "Create New Product"}</h2> */}
+            <form onSubmit={(event) => handleSubmit(event)}>
+            <Form.Group className="mb-3" controlId="product">
+                {/* <Form.Label>Sl Number</Form.Label> */}
+                <Form.Control className="d-flex" type="text" placeholder="SL Number" />
 
-                    <Form.Group as={Col} md="1" controlId="validationCustom04">
-                        <Form.Label>Details</Form.Label>
-                        <Form.Control type="text" name="details" placeholder="Details" defaultValue=""/>  
-                    </Form.Group>
+                <Form.Label>Sl Number</Form.Label>
+                <Form.Control type="text" placeholder="SL Number" />
+                <table className="table m-2 table-bordered table-hover">
+                    <thead>
+                    <tr className="">
+                        <th>Sl_No</th>
+                        <th>Sales_Purchase</th>
+                        <th>Date</th>
+                        <th>Party_Name</th>
+                        <th>Details</th>
+                        <th>Weight</th>
+                        <th>manpower</th>
+                        <th>Material</th>
+                        <th>Freight</th>
+                        <th>Maintainance</th>
+                        <th>Sales</th>
+                        <th>Payment_Received</th>
+                        <th>Amount_paid</th>
+                        <th>Balance</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
 
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Weight</Form.Label>
-                        <Form.Control type="text" name="weight" placeholder="Weight" defaultValue="" />
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Manpower</Form.Label>
-                        <Form.Control type="text" name="manpower" placeholder="Manpower" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Material</Form.Label>
-                        <Form.Control type="text" name="material" placeholder="Material" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Freight</Form.Label>
-                        <Form.Control type="text" name="freight" placeholder="Freight" defaultValue=""/>
-                    </Form.Group>
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Maintainance</Form.Label>
-                        <Form.Control type="text" name="maintainance" placeholder="Maintainance" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Sales</Form.Label>
-                        <Form.Control type="text" name="sales" placeholder="Sales" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Payment_Received</Form.Label>
-                        <Form.Control type="payment_received" name="Payment_Received" placeholder="Payment_Received" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Amount_Paid</Form.Label>
-                        <Form.Control type="text" name="amount_paid" placeholder="Amount_Paid" defaultValue=""/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Balance</Form.Label>
-                        <Form.Control type="text" name="balance" placeholder="Balance" defaultValue=""/>
-                    </Form.Group>
-
-                    {/* <Form.Group as={Col} md="1" controlId="validationCustom05">
-                        <Form.Label>Action</Form.Label>  
-                    </Form.Group> */}
-
-                    <Button type="submit" className="btn btn-success btn-sm me-2 mt-3 ">Save</Button>
-                    {props.showButton && (
-                            <button onClick={() => props.handleAdd()} type="button" className="btn btn-primary btn-sm me-2"> New</button>
-                    )}
-                </Row>
-                
-            </Form>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input className="form-control" id="sl_no" name="sl_no" defaultValue={props.product.sl_no} 
+                                />
+                            </td>
+                            <td>
+                                <input className="form-control" id="sales_purchase" name="sales_purchase" defaultValue={props.product.sales_purchase}  />
+                            </td>
+                            <td>
+                                <input className="form-control" id="date" name="date" defaultValue={props.product.date} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="party_name" name="party_name" defaultValue={props.product.party_name} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="details" name="details" defaultValue={props.product.details} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="weight" name="weight" defaultValue={props.product.weight} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="manpower" name="manpower" defaultValue={props.product.manpower} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="material" name="material" defaultValue={props.product.material} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="freight" name="freight" defaultValue={props.product.freight} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="maintainance" name="maintainance" defaultValue={props.product.maintainance} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="sales" name="sales" defaultValue={props.product.sales} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="payment_received" name="payment_received" defaultValue={props.product.payment_received} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="amount_paid" name="amount_paid" defaultValue={props.product.amount_paid} />
+                            </td>
+                            <td>
+                                <input className="form-control" id="balance" name="balance" defaultValue={props.product.balance} />
+                            </td>
+                            <td style={{ width: "10px", whiteSpace: "nowrap" }}>
+                                <button type="submit" className="btn btn-success btn-sm me-2 ">Save</button>
+                                {props.showButton && (
+                                    <button onClick={() => props.handleAdd()} type="button" className="btn btn-primary btn-sm me-2"> New</button>
+                                )}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Form.Group>
+            </form>
         </>
-    );
+    )
 }
-
-    
-    
